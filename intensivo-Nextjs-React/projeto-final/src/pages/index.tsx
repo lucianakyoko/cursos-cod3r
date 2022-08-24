69bb4d8e54b25824df2dbc6f67b5e7e543748c00
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-08-24 16:19:27 -0300

    feat(intensivo-Nextjs-React/projeto-final): class 10/18 - Componente Formulário

--- intensivo-Nextjs-React/projeto-final/src/pages/index.tsx
+++ intensivo-Nextjs-React/projeto-final/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from '../components/Botao';
+import Formulario from '../components/Formulario';
 import Layout from '../components/Layout';
 import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente';
@@ -33,6 +34,7 @@ export default function Home() {
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
         />
+        <Formulario cliente={clientes[0]}></Formulario>
       </Layout>
     </div>
   )
