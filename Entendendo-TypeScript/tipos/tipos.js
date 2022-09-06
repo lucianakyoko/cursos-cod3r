5bdb1fb22a2e38426b0d787f7445a56ab8a99ed2
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2022-09-06 19:51:15 -0300

    feat(Entendendo-TypeScript/tipos): add cap 2 class 03/22 - Tipos Numericos e Booleano

--- Entendendo-TypeScript/tipos/tipos.js
+++ Entendendo-TypeScript/tipos/tipos.js
@@ -3,3 +3,13 @@
 let nome = 'João';
 console.log(nome);
 // nome = 28;
+//numbers
+let idade = 27;
+// idade = 'Ana';
+idade = 29;
+idade = 29.2;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies);
--- Entendendo-TypeScript/tipos/tipos.ts
+++ Entendendo-TypeScript/tipos/tipos.ts
@@ -1,5 +1,16 @@
 //string
 let nome = 'João';
 console.log(nome);
-
 // nome = 28;
+
+//numbers
+let idade = 27;
+// idade = 'Ana';
+idade = 29;
+idade = 29.2;
+console.log(idade)
+
+//boolean
+let possuiHobbies = false;
+// possuiHobbies = 1;
+console.log(possuiHobbies)
